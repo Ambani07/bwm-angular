69d6a1a4583696424904ea39f7481aaa2cd5d959
Ambani Matsedu <ambani.matsedu@gmail.com>
2018-12-17 12:54:09 +0200

    Add header component and scss styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bwm';
+  componentTitle = 'I am component title';
+
+  clickHandler(){
+    alert("I AM CLICKED");
+  }
 }
+
